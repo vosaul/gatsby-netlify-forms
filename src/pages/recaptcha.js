17ed7968d7380f472b9acdce653ab11608152aee
vosaul <v986963@gmail.com>
2021-04-10 15:52:55 +0300

    +++

--- src/pages/recaptcha.js
+++ src/pages/recaptcha.js
@@ -3,7 +3,7 @@ import { navigate } from 'gatsby'
 import Recaptcha from 'react-google-recaptcha'
 import Layout from '../layout'
 
-//const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
+const RECAPTCHA_KEY1 = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
 const RECAPTCHA_KEY = "6LcfwqEaAAAAAAsopYYS3eDqx08Y6kGKbb49O5wu";
 if (typeof RECAPTCHA_KEY === 'undefined') { console.log("Error!")
   /* throw new Error(`
@@ -13,7 +13,7 @@ if (typeof RECAPTCHA_KEY === 'undefined') { console.log("Error!")
   Note this demo is specifically for Recaptcha v2
   `) */
 }
-console.log(process.env.SITE_RECAPTCHA_KEY);
+console.log(RECAPTCHA_KEY1);
 function encode(data) {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
